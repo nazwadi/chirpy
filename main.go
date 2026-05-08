dec8d3cf8b5599012985f87d099509f58fd00f76
Nazwadi <nazwadi@pm.me>
2026-05-07 22:52:07 -0400

    Complete up to Middleware Lesson

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.Handle("/app/", http.StripPrefix("/appt ", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			return
+		}
+	})
 	fmt.Println("Listening on port 8080...")
 	err := myServer.ListenAndServe()
 	if err != nil {
