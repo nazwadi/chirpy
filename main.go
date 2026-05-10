2e8aac1d219a9f25135a5452e275ee2ed05870ec
Nazwadi <nazwadi@pm.me>
2026-05-10 14:03:32 -0400

    Completes Stateful Handlers lesson

--- main.go
+++ main.go
@@ -4,15 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	s := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+	_, err := w.Write([]byte(s))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	myServer := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/appt ", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
@@ -21,6 +49,8 @@ func main() {
 			return
 		}
 	})
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 	fmt.Println("Listening on port 8080...")
 	err := myServer.ListenAndServe()
 	if err != nil {
